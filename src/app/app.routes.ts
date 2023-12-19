d001cd780c921ecddd05d10eb8e0b8b62a137c44
unknown <davedalimocon@gmail.com>
2023-12-19 20:34:41 +0800

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from '../pages/home/home.component';
+import { AboutComponent } from '../pages/about/about.component';
+import { ContactsComponent } from '../pages/contacts/contacts.component';
+
+export const routes: Routes = [
+    { path:'home-component', component: HomeComponent},
+    { path:'about-component', component: AboutComponent},
+    { path:'contacts-component', component: ContactsComponent},
+];
--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
